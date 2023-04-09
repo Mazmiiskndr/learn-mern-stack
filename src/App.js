64a2d1402b961927b00af15c4e3867d75570b8a7
Moch Azmi Iskandar <azmiiskandar0@gmail.com>
2023-04-09 16:30:21 +0700

    :sparkles: (feat): create ui Header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "assets/scss/style.scss";
+import LandingPage from "pages/LandingPage";
+
 function App() {
   return (
     <div className="App">
-      Hello World
-    
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
